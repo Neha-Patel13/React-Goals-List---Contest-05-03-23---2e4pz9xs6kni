ef73782e93c951c7ad615b0d7ce78443e262fc57
Neha-Patel13 <122043318+Neha-Patel13@users.noreply.github.com>
2023-03-05 15:05:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+import GoalForm from "./GoalForm";
+import ListOfGoals from "./ListOfGoals";
 
+const App = () => {
+  const [allGoals, setAllGoals] = useState([]);
+
+  const addGoal = (newGoal) => {
+    setAllGoals([...allGoals, newGoal]);
+  };
+
+  return (
+    <div id="main">
+      <GoalForm addGoal={addGoal} />
+      <ListOfGoals goals={allGoals} />
+    </div>
+  );
+};
+
+export default App;
